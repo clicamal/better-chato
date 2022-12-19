0ea4d7a3c0f89ffa6c48a36cdfc62896140aa049
moccot <justother94@gmail.com>
2022-12-19 01:35:12 +0000

    add authentication forms

--- vue-server/src/LoginForm.vue
+++ vue-server/src/LoginForm.vue
@@ -1,8 +1,10 @@
 <template>
-    <form id="register-form" class="pure-form">
+    <form id="login-form" class="pure-form authentication-form">
+        <h3 id="login-form-heading">Login Form</h3>
+
         <input type="text" name="username" id="username-input" placeholder="Username">
         <input type="password" name="password" id="password-input" placeholder="Password">
-        <button type="submit">Register</button>
+        <button type="submit" class="pure-button pure-button-active">Login</button>
     </form>
 </template>
 
@@ -10,13 +12,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'RegisterForm'
+    name: 'LoginForm'
 });
 </script>
-
-<style scoped>
-form#register-form {
-    background-color: var(--primary-color);
-    margin: 25% 10px;
-}
-</style>
---
+++ vue-server/src/RegisterForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <form id="registration-form" class="pure-form authentication-form">
+        <h3 id="registration-form-heading">Registration Form</h3>
+
+        <input type="text" name="username" id="username-input" placeholder="Username">
+        <input type="password" name="password" id="password-input" placeholder="Password">
+        <button type="submit" class="pure-button pure-button-active">Register</button>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'RegisterForm'
+});
+</script>
