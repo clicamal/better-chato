a4a1cc35715c8d032af622490c6d1c3f9e3f4c41
moccot <justother94@gmail.com>
2022-12-19 21:27:07 +0000

    prepare laravel-server for user authentication

---
+++ laravel-server/app/Http/Controllers/UsersController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function create(Request $request)
+    {
+        $userData = $request->validate([
+            'username' => 'required|unique:users|alpha_num|max:12',
+            'password' => 'required'
+        ]);
+
+        User::create([
+            'username' => $userData['username'],
+            'password' => Hash::make($userData['password'])
+        ]);
+
+        return response([
+            'message' => 'User created successfully.'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $userData = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt([ 'username' => $userData['username'], 'password' => $userData['password'] ])) {
+            $request->session()->regenerate();
+
+            return response([
+                'message' => 'User logged in successfully.'
+            ]);
+        }
+    }
+}
